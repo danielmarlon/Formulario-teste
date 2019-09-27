63bbff6172a638dd4351c118154bf03effa0704e
Daniel <danielmarlon181@gmail.com>
2019-09-27 20:58:55 -0300

    updating

--- formulario.php
+++ formulario.php
@@ -1,22 +1,41 @@
-<form action="/Cofre.php" method="post">
-<div>
-	<label for="Nome">Nome:</label>
-	<input type="text" id="Nome" name="Nome" />
-</div>
-<div>
-	<label for="Idade">Idade:</label>
-	<input type="text" id="Idade" name="Idade" />
-</div>
-<div>
-	<label for="Endereço">Endereço:</label>
-	<input type="text" id="Endereço" name="Endereço" />
-</div>
-<div>
-	<label for="Possui alguma deficiência?">Possui alguma deficiência?</label>
-	<input type="radio" name="op" value="Sim"> Sim 
-	<input type="radio" name="op" value="Não"> Não
-</div>
+<!DOCTYPE html>
+<html
+	<head>
+		<title>Formulário-teste</title>
+		<meta name="description" content="Criação se site-teste com um formulário" />
+	</head>
+	<body bgcolor="Red" text="Blue">
+	<form name="Formulario" action="/Cofre.php" method="post">
+		<table>
+			<caption>CADASTRO</caption>
+			<dir>
+			<tr><td>
+				<label for="Nome">Nome:</label></td>
+				<td><input type="text" id="Nome" name="Nome" /> </td></tr>
+			</dir>
+			<dir>
+			<tr><td>
+				<label for="Idade">Idade:</label></td>
+				<td><input type="text" id="Idade" name="Idade" /> </td></tr>
+			</dir>
+			<dir>
+			<tr><td>
+				<label for="Endereço">Endereço:</label></td>
+				<td><input type="text" id="Endereço" name="Endereço" /></td></tr>
+			</dir>
+			<dir>
+				<tr><td>
+				<label for="Possui alguma deficiência?">Possui alguma deficiência?</label></td>
+				<td><input type="radio" name="op" value="Sim"> Sim
+				<input type="radio" name="op" value="Não"> Não </td></tr>
+			</div>
+			<dir>
+
+			</div>
+		</table>
+		</form>
 
 
 
-</form>
+	</body>
+</html>
