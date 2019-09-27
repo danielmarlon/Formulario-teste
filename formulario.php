2c6a6e45526d227ee948e12febca4e570e79e7c9
Gabriel <gabrieladelario686@gmail.com>
2019-09-27 15:06:56 -0300

    first commit

---
+++ formulario.php
@@ -0,0 +1,22 @@
+<form action="/Cofre.php" method="post">
+<div>
+	<label for="Nome">Nome:</label>
+	<input type="text" id="Nome" name="Nome" />
+</div>
+<div>
+	<label for="Idade">Idade:</label>
+	<input type="text" id="Idade" name="Idade" />
+</div>
+<div>
+	<label for="Endereço">Endereço:</label>
+	<input type="text" id="Endereço" name="Endereço" />
+</div>
+<div>
+	<label for="Possui alguma deficiência?">Possui alguma deficiência?</label>
+	<input type="radio" name="op" value="Sim"> Sim 
+	<input type="radio" name="op" value="Não"> Não
+</div>
+
+
+
+</form>
