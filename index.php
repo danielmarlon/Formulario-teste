19fd4548f563d595b54eb4ff144c233be1cf97d8
Daniel <danielmarlon181@gmail.com>
2019-10-21 14:07:45 -0300

    switch tab to space

--- index.php
+++ index.php
@@ -11,120 +11,120 @@
  <dir>
   <tr>
   <td>
-	 <label for="Name">Name:</label>
+   <label for="Name">Name:</label>
   </td>
-	<td>
-   <input type="text" id="Name" name="Name" required />*
-	</td>
+  <td>
+    <input type="text" id="Name" name="Name" required />*
+  </td>
   </tr>
  </dir>
  <dir>
   <tr>
   <td>
-	 <label for="Age">Age:</label>
+   <label for="Age">Age:</label>
   </td>
-	<td>
+  <td>
    <input type="text" id="Age" name="Age" required  />*
-	</td>
+  </td>
   </tr>
  </dir>
  <dir>
   <tr>
   <td>
-	 <label for="Gender">Gender:</label>
+   <label for="Gender">Gender:</label>
   </td>
-	<td>
+  <td>
    <input type="radio" id="Gender" name="Gender" value="Male" required /> Male
-	 <input type="radio" id="Gender" name="Gender" value="Female" required /> Female *
-	</td>
+   <input type="radio" id="Gender" name="Gender" value="Female" required /> Female *
+  </td>
   </tr>
  </dir>
  <dir>
-	<tr>
+  <tr>
   <td>
-	 <label for="Address">Address:</label>
+   <label for="Address">Address:</label>
   </td>
-	<td>
+  <td>
    <input type="text" id="Address" name="Address" required />*
-	</td>
+  </td>
   </tr>
  </dir>
  <dir>
-	<tr>
+  <tr>
   <td>
-	 <label for="City">City:</label>
+   <label for="City">City:</label>
   </td>
-	<td>
+  <td>
    <input type="text" id="City" name="City" required />*
-	</td>
+  </td>
   </tr>
  </dir>
  <dir>
-	<tr>
+  <tr>
   <td>
-	 <label for="State">State:</label>
+   <label for="State">State:</label>
   </td>
-	<td>
+  <td>
    <select class="State" name="State" id="State" required >
-		<option>Selecione...</option>
-		<option value="ac">AC</option>
-		<option value="al">AL</option>
-		<option value="ap">AP</option>
-		<option value="am">AM</option>
-		<option value="ba">BA</option>
-		<option value="ce">CE</option>
-		<option value="es">ES</option>
-		<option value="df">DF</option>
-		<option value="ma">MA</option>
-		<option value="mt">MT</option>
-		<option value="ms">MS</option>
-		<option value="mg">MG</option>
-		<option value="pa">PA</option>
-		<option value="pb">PB</option>
-		<option value="pr">PR</option>
-		<option value="pe">PE</option>
-		<option value="pi">PI</option>
-		<option value="rj">RJ</option>
-		<option value="rn">RN</option>
-		<option value="rs">RS</option>
-		<option value="ro">RO</option>
-		<option value="rr">RR</option>
-		<option value="sc">SC</option>
-		<option value="sp">SP</option>
-		<option value="se">SE</option>
-	  <option value="to">TO</option>
-	 </select>*
+    <option>Selecione...</option>
+    <option value="ac">AC</option>
+    <option value="al">AL</option>
+    <option value="ap">AP</option>
+    <option value="am">AM</option>
+    <option value="ba">BA</option>
+    <option value="ce">CE</option>
+    <option value="es">ES</option>
+    <option value="df">DF</option>
+    <option value="ma">MA</option>
+    <option value="mt">MT</option>
+    <option value="ms">MS</option>
+    <option value="mg">MG</option>
+    <option value="pa">PA</option>
+    <option value="pb">PB</option>
+    <option value="pr">PR</option>
+    <option value="pe">PE</option>
+    <option value="pi">PI</option>
+    <option value="rj">RJ</option>
+    <option value="rn">RN</option>
+    <option value="rs">RS</option>
+    <option value="ro">RO</option>
+    <option value="rr">RR</option>
+    <option value="sc">SC</option>
+    <option value="sp">SP</option>
+    <option value="se">SE</option>
+    <option value="to">TO</option>
+   </select>*
   </td>
   </tr>
  </dir>
  <dir>
   <tr>
   <td>
-	 <label for="District">District:</label>
+   <label for="District">District:</label>
   </td>
-	<td>
+  <td>
    <input type="text" name="District" id="District" required/>*
-	</td>
+  </td>
   </tr>
  </dir>
  <dir>
   <tr>
   <td>
-	 <label for="Country">Country:</label>
+   <label for="Country">Country:</label>
   </td>
-	<td>
+  <td>
    <input type="text" name="Country" id="Country" required />*
-	</td>
+  </td>
   </tr>
  </dir>
  <dir>
   <tr>
   <td>
-	 <label for="E-mail">E-mail:</label>
+   <label for="E-mail">E-mail:</label>
   </td>
-	<td>
+  <td>
    <input type="email" id="E-mail" name="E-mail" />
-	</td>
+  </td>
   </tr>
  </dir>
  <dir>
@@ -132,52 +132,52 @@
   <td>
    <label for="Login">Login:</label>
   </td>
-	<td>
+  <td>
    <input type="text" name="Login" id="Login" maxlength="10" required />*
-	</td>
+  </td>
   </tr>
  </dir>
  <dir>
   <tr>
   <td>
-	 <label for="Password">Password:</label>
+   <label for="Password">Password:</label>
   </td>
-	<td>
+  <td>
    <input type="password" name="Password" id="Password" maxlength="10" required />*
-	</td>
+  </td>
   </tr>
  </dir>
  <dir>
   <tr>
   <td>
-	 <label for="op">Civil Status</label>
+   <label for="op">Civil Status</label>
   </td>
-	<td>
+  <td>
    <input type="radio" name="op" value="Married" /> Married
-	 <input type="radio" name="op" value="Single" /> Single
-	 <input type="radio" name="op" value="Widower" /> Widower
-	 <input type="radio" name="op" value="Divorced" /> Divorced
+   <input type="radio" name="op" value="Single" /> Single
+   <input type="radio" name="op" value="Widower" /> Widower
+   <input type="radio" name="op" value="Divorced" /> Divorced
   </td>
   </tr>
  </dir>
  <div>
   <tr>
   <td>
-	 <label for="Comments">Comments:</label>
+   <label for="Comments">Comments:</label>
   </td>
-	<td>
+  <td>
    <textarea name="Comments" id="Comments" rows="8" cols="80"></textarea>
-	</td>
+  </td>
   </tr>
  </div>
  <div>
   <tr>
   <td>
-	 <input type="submit" name="Register" id="Register" value="Submit" />
+   <input type="submit" name="Register" id="Register" value="Submit" />
   </td>
-	<td>
+  <td>
    <input type="reset" name="Reset" id="Reset" value="Reset" />
-	</td>
+  </td>
   </tr>
  </div>
 </table>
